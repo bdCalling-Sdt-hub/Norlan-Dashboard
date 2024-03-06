39eccf45f69687cfd5cf402e3b2e0d1985537628
Shehabuddin-Tushar <freelancerrtushar@gmail.com>
2024-03-06 09:37:54 +0600

    some changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.10.102",
+    host: "192.168.10.13",
     port: "3001",
   },
 });
