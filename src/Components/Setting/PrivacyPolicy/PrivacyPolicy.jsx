69602e673a94fb8679dcfcff9df6efc478b9896f
Nadir Hossain <nadirhossain336@gmail.com>
2024-04-19 15:01:17 +0600

    :sparkles: privacy and policy  retrive data and  update  api intrigation

--- src/Components/Setting/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/Components/Setting/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,16 +1,63 @@
-import React, { useState, useRef, useMemo } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import JoditEditor from 'jodit-react';
 import { Button, Col, Row } from 'antd';
+import baseURL from '../../../../baseURL';
+import Swal from 'sweetalert2';
 
 const PrivacyPolicy = () => {
 
+  const [data, setData] = useState()
   const editor = useRef(null)
   const [content, setContent] = useState('');
+  const [refreash, setRefreash] = useState('')
 
-  const aboutDataSave = () => {
-    alert(content);
+  if(refreash){
+    setTimeout(()=>{
+      setRefreash("")
+    },[1500])
+  }
+
+
+  const aboutDataSave =async () => {
+    await baseURL.patch(`/privacy/update-privacy/${data._id}`, {description: content}, {
+      headers: {
+        "Content-Type": "application/json",
+        authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
+      }
+    }).then((response)=>{
+      console.log(response)
+      if(response.status === 200){
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          width: 550,
+          title: response.data.message,
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    })
 
   }
+  useEffect(()=>{
+    setContent(data?.description);
+  }, [data])
+
+  useEffect(()=>{
+    async function getApi(){
+      await baseURL.get("/privacy/get-privacy", {
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
+        }
+      }).then((response)=>{
+        if(response.status === 200){
+          setData(response.data.data)
+        }
+      })
+    }
+    getApi();
+  }, [refreash !== ""]);
   return (
     <div>
       
