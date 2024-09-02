d2650c3d93a0703a500e8272bb2ce3ba4b7df56d
siffahim <siffahim25@gmail.com>
2024-09-02 12:40:09 +0600

    :sparklse:

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    commonJsOptions: { transformMixedEsModules: true }
-  },
   server: {
-    host: "104.248.15.129",
-    port: "3000",
+    host: "127.0.0.1",
+    port: 3000,
   },
 });
